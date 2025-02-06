e94306c4cb40f9e588de111e72c04bc22d9dbfd0
Alex Bosco <ajbosco00@gmail.com>
2025-02-05 20:27:48 -0500

    test

--- home/base.nix
+++ home/base.nix
@@ -1,1 +1,12 @@
-{...}:{}
+{dotfiles, ...}:{
+
+ xdg = {
+   configFile."tmux/tmux.conf".source = "${dotfiles}/tmux.conf";
+   configFile."nvim/".source = "${dotfiles}/nvim/";
+   configFile."sway/".source = "${dotfiles}/sway/";
+   configFile."waybar/".source = "${dotfiles}/waybar/";
+   configFile."gtk-4.0/".source = "${dotfiles}/gtk-4.0/";
+ };
+
+ xdg.userDirs.desktop = "$HOME/";
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -111,15 +111,5 @@
 
   hardware.enableRedistributableFirmware = true;
 
- xdg = {
-   configFile."tmux/tmux.conf".source = "${dotfiles}/tmux.conf";
-   configFile."nvim/".source = "${dotfiles}/nvim/";
-   configFile."sway/".source = "${dotfiles}/sway/";
-   configFile."waybar/".source = "${dotfiles}/waybar/";
-   configFile."gtk-4.0/".source = "${dotfiles}/gtk-4.0/";
- };
-
- xdg.userDirs.desktop = "$HOME/";
-
-  environment.etc.tmux.source = "${dotfiles}/tmux";
+  # environment.etc.tmux.source = "${dotfiles}/tmux";
 }
