d5bc858520319513320ab3271df4664625c40945
Alex Bosco <ajbosco00@gmail.com>
2025-02-08 20:25:17 -0500

    test

--- home/modules/desktop/sway.nix
+++ home/modules/desktop/sway.nix
@@ -1,4 +1,12 @@
 { config, pkgs, inputs, lib,... }:
 {
   wayland.windowManager.sway.enable = true;
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "wayland-session" ''
+      /run/current-system/systemd/bin/systemctl --user start graphical-session.target
+      dbus-run-session "$@"
+      /run/current-system/systemd/bin/systemctl --user stop graphical-session.target
+    '')
+  ];
 }
